90facdcc278d42f9a612e80ff7634bb04e3e9b9c
Rishi Chandna <rishirules01@gmail.com>
2021-02-28 00:47:42 -0600

    added static map files

---
+++ points.js
@@ -0,0 +1,327 @@
+eqfeed_callback({
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)777-7777"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.73230171203613,
+          32.95920171053244
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.74509048461913,
+          32.95091937426127
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.75212860107422,
+          32.96661914407465
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.72586441040039,
+          32.982748111760046
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.78302764892578,
+          32.95192770016952
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Landscaping",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.72732353210448,
+          32.941051578030795
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.78508758544922,
+          32.93471255866877
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Landscaping",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7898941040039,
+          32.99081149087488
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Carpenter",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.81289672851562,
+          32.93550496093696
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Carpenter",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70269012451172,
+          32.97727611361216
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7123031616211,
+          32.96273047016416
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Carpenter",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71316146850585,
+          32.94054735448843
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Carpenter",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.72637939453125,
+          32.91374733907198
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.6357421875,
+          32.916197139908846
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Landscaping",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.76380157470702,
+          32.917349963900215
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.80122375488281,
+          32.893714071222995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.84516906738281,
+          32.96489086569023
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Landscaping",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.67041778564452,
+          32.9576893418361
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.84173583984374,
+          32.95048723106514
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Electrician",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.63436889648438,
+          32.944437004496464
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Roofing",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.79882049560547,
+          32.81642107033251
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Roofing",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7507553100586,
+          32.799396032368655
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Service": "Plumber",
+        "Contact Info": "(512)666-6666"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.73633575439453,
+          32.88333534513211
+        ]
+      }
+    }
+  ]
+});
